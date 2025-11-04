2fec75316bc46e47c08455e9880731f53d33da35
Eicon <lnemixg@gmail.com>
2025-11-04 20:51:54 +0500

    feat(TaskForm): add description input field and logic

--- stardeck-react/src/components/TaskForm.tsx
+++ stardeck-react/src/components/TaskForm.tsx
@@ -1,8 +1,9 @@
-import {useState} from "react";
-import type {TaskCycle, Priority} from "../types/task.ts";
+import React, {useState} from "react"; //добавил React чтоб IDE не жаловалась
+import type {TaskCycle, Priority, Description} from "../types/task.ts";
+import s from "./TaskForm.module.css";
 
 type Props = {
-    onAdd: (title: string, priority: Priority, kind: TaskCycle) => void;
+    onAdd: (title: string, description: Description, priority: Priority, kind: TaskCycle) => void;
     dailyCount?: number;
 };
 
@@ -10,17 +11,21 @@ type Props = {
 
 export default function TaskForm({onAdd}: Props) {
     const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
     const [priority, setPriority] = useState<Priority>('med');
     const [kind, setKind] = useState<TaskCycle>('daily');
 
     function submit(event: React.FormEvent) {
         event.preventDefault();
         const t = title.trim();
+        const d = description.trim() || null;
         if (!t) return;
-        onAdd(t, priority, kind);
+        onAdd(t, d, priority, kind);
         setTitle('');
+        setDescription('');
         setPriority('med');
         setKind('daily');
+        console.log(d);
     }
 
     function handleTitleChange(event: React.ChangeEvent<HTMLInputElement>) {
@@ -34,29 +39,43 @@ export default function TaskForm({onAdd}: Props) {
     function handleSelectKindChange(event: React.ChangeEvent<HTMLSelectElement>) {
         setKind(event.target.value as TaskCycle);
     }
+
+    function handleDescriptionChange(event: React.ChangeEvent<HTMLInputElement>) {
+        setDescription(event.target.value);
+    }
     //Нужны ли приоритеты для daily? скорее да чем нет
+    //вынести стили, стили нужно централизовывать для удобства и масштабируемости
     return (
-        <form onSubmit={submit} style={{display: 'grid', gap: 8, gridTemplateColumns: '1fr auto auto auto'}}>
-            <input
-                placeholder='Новая задача'
-                value={title}
-                onChange={handleTitleChange}
-                aria-label='Название задачи'
-            />
-            <select value={priority}
-                    onChange={handlePriorityChange}
-                    aria-label='Приоритет'>
-                <option value='low'>Низкий</option>
-                <option value='med'>Средний</option>
-                <option value='high'>Высокий</option>
-            </select>
-            <select value={kind}
-                    onChange={handleSelectKindChange}
-                    aria-label='Тип задачи'>
-                <option value='daily'>Ежедневная</option>
-                <option value='long'>Долговременная</option>
-            </select>
-            <button type='submit' disabled={!title.trim()}>Добавить</button>
-        </form>
+        <div className={s.toolbar}>
+            <form onSubmit={submit} style={{display: 'grid', gap: 8, gridTemplateColumns: '1fr auto auto auto'}}>
+                <input
+                    placeholder='Новая задача'
+                    value={title}
+                    onChange={handleTitleChange}
+                    aria-label='Название задачи'
+                />
+                <input
+                    placeholder='Описание (необязательно)'
+                    value={description}
+                    onChange={handleDescriptionChange}
+                    aria-label='Описание задачи'
+                />
+                <select value={priority}
+                        onChange={handlePriorityChange}
+                        aria-label='Приоритет'>
+                    <option value='low'>Низкий</option>
+                    <option value='med'>Средний</option>
+                    <option value='high'>Высокий</option>
+                </select>
+                <select value={kind}
+                        onChange={handleSelectKindChange}
+                        aria-label='Тип задачи'>
+                    <option value='daily'>Ежедневная</option>
+                    <option value='long'>Долговременная</option>
+                </select>
+                <button type='submit' disabled={!title.trim()}>Добавить</button>
+            </form>
+        </div>
+
     );
 }
