a9b81cd4e7d15c53fd12d3c40e61ff6b41b65eb6
Eicon <lnemixg@gmail.com>
2025-11-04 20:58:19 +0500

    chore(types): add Description type and update Task interface

--- stardeck-react/src/types/task.ts
+++ stardeck-react/src/types/task.ts
@@ -1,9 +1,11 @@
 export type Priority = 'low' | 'med' | 'high';
 export type TaskCycle = 'daily' | 'long';
+export type Description = string | null;
 
 export type Task = {
   id: string;
   title: string;
+  description: Description;
   done: boolean;
   createdAt: number;
   priority: Priority;
