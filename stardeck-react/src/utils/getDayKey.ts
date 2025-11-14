f753b730b9ea07f69346effa2cd43fd490c8e2f4
Eicon <lnemixg@gmail.com>
2025-11-14 23:28:14 +0500

    refactor(utils): lite refactor getDayKey

--- stardeck-react/src/utils/getDayKey.ts
+++ stardeck-react/src/utils/getDayKey.ts
@@ -1,8 +1,8 @@
 export default function getDayKey(date: Date = new Date()) {
 
-    const year  = String(date.getFullYear());
+    const year = String(date.getFullYear());
     const month = String(date.getMonth() + 1).padStart(2, '0');
-    const day   = String(date.getDate()).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
 
     return year + month + day;
 };
