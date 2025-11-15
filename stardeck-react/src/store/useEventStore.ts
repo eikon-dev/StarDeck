406688f30399352f40173c2313c8bce969b3a01d
Eicon <lnemixg@gmail.com>
2025-11-15 23:16:41 +0500

    feat(store): add EventStore for LongRewardEngine

---
+++ stardeck-react/src/store/useEventStore.ts
@@ -0,0 +1,39 @@
+import {create} from "zustand";
+import type {TaskCycle} from "../types/task.ts";
+
+interface EventStore {
+    events: TaskEvent[],
+    emitTaskToggled: (id: string, done: boolean, cycle: TaskCycle) => void,
+}
+
+type TaskEvent = {
+    id: string,
+    done: boolean,
+    cycle: TaskCycle,
+    timestamp: number,
+    type: 'task-toggled',
+}
+
+const useEventStore = create<EventStore>((set) => ({
+    events: [],
+
+
+
+    emitTaskToggled: (id, done, cycle) => {
+        const taskEvent: TaskEvent = {
+            id: id,
+            done: done,
+            cycle: cycle,
+            timestamp: Date.now(),
+            type: 'task-toggled',
+        }
+
+        set(s => ({
+            events: [...s.events, taskEvent],
+        }))
+    },
+
+
+}))
+
+export default useEventStore;
