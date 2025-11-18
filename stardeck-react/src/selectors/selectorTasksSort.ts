cb25848c811bde73855e19fdc86eba103b5c1ebc
Eicon <lnemixg@gmail.com>
2025-11-18 22:20:48 +0500

    feat(selector): add selector for sort daily and long tasks

---
+++ stardeck-react/src/selectors/selectorTasksSort.ts
@@ -0,0 +1,28 @@
+import type {Task} from "../types/task.ts";
+import {useMemo} from "react";
+
+interface TasksSort {
+    daily: Task[],
+    long: Task[],
+}
+
+export default function selectorTasksSort(tasks: Task[]): TasksSort {
+    const {daily, long} = useMemo(() => {
+        const daily: Task[] = [];
+        const long: Task[] = [];
+
+        for (const t of tasks) {
+            (t.cycle === 'daily' ? daily : long).push(t);
+        }
+
+        const sort = (a: Task, b: Task) =>
+            Number(a.done) - Number(b.done) || b.createdAt - a.createdAt;
+
+        daily.sort(sort);
+        long.sort(sort);
+
+        return {daily, long};
+    }, [tasks])
+
+    return {daily, long};
+}
