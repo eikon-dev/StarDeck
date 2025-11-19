d73208b1c71f8aa109c654c61ba286390aa0704e
Eicon <lnemixg@gmail.com>
2025-11-19 18:22:49 +0500

    refactor(types): change dayKey type from number to string

--- stardeck-react/src/types/rewards.ts
+++ stardeck-react/src/types/rewards.ts
@@ -3,7 +3,7 @@ export type RewardKind = 'daily-all' | 'long-complete';
 export type StarReward = {
     id: string;
     createdAt: number;
-    dayKey?: number;
+    dayKey?: string;
     kind: RewardKind;
     amount: number;
     taskId?: string;
