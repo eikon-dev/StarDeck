ff56e4a190ba171eb0c79dca56c15d142bd829b7
Eicon <lnemixg@gmail.com>
2025-11-21 20:40:52 +0500

    refactor(app): lite refactor App

--- stardeck-react/src/App.tsx
+++ stardeck-react/src/App.tsx
@@ -1,63 +1,16 @@
-import {useState} from "react";
-import TaskForm from "./components/TaskForm.tsx";
-import TaskList from "./components/TaskList.tsx";
-import TaskFilter from "./components/TaskFilter.tsx";
-import type {Task, Priority} from "./types/task.ts";
 import './index.css';
-import {useTaskStats} from "./hooks/useTaskStats.ts";
-import {useTaskFilter} from "./hooks/useTaskFilter.ts";
+// import s from "./App.module.css";
+import RewardEngine from "./engines/RewardEngine.tsx";
+import StarDeckScene from "./components/NewUIDev/scene/StarDeckScene.tsx";
 
+// Уже внедряю графику
 
-//debounce для localStorage нужно попробовать
-//так же попробовать debounce для инпутов
 export default function App() {
-    const [tasks, setTasks] = useState<Task[]>([]);
-    const [filter, setFilter] = useState<Priority | 'all'>('all');
-
-    function addTask(title: string, priority: Priority) {
-        const t: Task = {
-            id: crypto.randomUUID?.() ?? String(Date.now() + Math.random()),
-            title,
-            done: false,
-            createdAt: Date.now(),
-            priority,
-        };
-        setTasks(prev => [t, ...prev]);
-    }
-
-    function handleFilterChange(priority: Priority | 'all') {
-        setFilter(priority);
-    }
-
-    function toggleTask(id: string) {
-        setTasks(prev => prev.map(t => (t.id === id ? {...t, done: !t.done} : t)));
-    }
-
-    function removeTask(id: string) {
-        setTasks(prev => prev.filter(t => t.id !== id));
-    }
-
-    const stats = useTaskStats(tasks); //My fist hook :) Горжусь
-
-    const filteredTasks = useTaskFilter(tasks, filter);
-    // вынести стили отдельно
     return (
-        <div style={{maxWidth: 720, margin: '40px auto', padding: '0 16px'}}>
-            <header
-                style={{display: 'flex', justifyContent: 'space-between', alignItems: 'baseline', marginBottom: 12}}>
-                <h1>StarDeck</h1>
-                <small style={{opacity: 0.7}}>{stats.done}/{stats.total} • {stats.pct}%</small>
-            </header>
+        <>
+            <RewardEngine/>
 
-            <TaskForm onAdd={addTask}/>
-
-            <div style={{height: 12}}/>
-            <TaskFilter onFilterChange={handleFilterChange}/>
-            <TaskList
-                item={filteredTasks}
-                onToggle={toggleTask}
-                onRemove={removeTask}/>
-        </div>
+            <StarDeckScene/>
+        </>
     );
 }
-
