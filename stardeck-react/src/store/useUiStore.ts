76b35ccf157bd8ba3dbc6e4ac5baffb98f484455
Eicon <lnemixg@gmail.com>
2025-11-24 22:27:04 +0500

    feat(store): add isTaskFormOpen

--- stardeck-react/src/store/useUiStore.ts
+++ stardeck-react/src/store/useUiStore.ts
@@ -2,15 +2,24 @@ import {create} from "zustand";
 
 interface UiStore {
     isOpenPanel: boolean;
+    isTaskFormOpen: boolean;
     togglePanel: () => void;
+    toggleTaskForm: () => void;
 }
 
 const useUiStore = create<UiStore>((set) => ({
     isOpenPanel: false,
+    isTaskFormOpen: false,
 
     togglePanel: () => {
         set(s => ({
             isOpenPanel: !s.isOpenPanel
+        }))
+    },
+
+    toggleTaskForm: () => {
+        set(s => ({
+            isTaskFormOpen: !s.isTaskFormOpen
         }))
     },
 }))
