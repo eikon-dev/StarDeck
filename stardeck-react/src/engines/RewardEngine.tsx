9a4175841f3c82a32f6339a606de6939db568219
Eicon <lnemixg@gmail.com>
2025-11-30 22:21:16 +0500

    chore(engine): add inline comment Reward Engine

--- stardeck-react/src/engines/RewardEngine.tsx
+++ stardeck-react/src/engines/RewardEngine.tsx
@@ -1,6 +1,6 @@
 import useLongRewardEngine from "./useLongRewardEngine.ts";
 import useDailyRewardEngine from "./useDailyRewardEngine.ts";
-
+//reward-pipeline когда наград станет больше
 export default function RewardEngine() {
     useLongRewardEngine();
     useDailyRewardEngine();
