b2f593d53a4f1783db72bc4c9b90e300f51528de
Eicon <lnemixg@gmail.com>
2025-12-11 21:21:55 +0500

    feat(ui): add Header component

---
+++ stardeck-react/src/components/scene/Header.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Header() {
+    return(
+        <header className="flex flex-col items-center font-headline text-3xl">
+            Star Deck
+        </header>
+    );
+}
