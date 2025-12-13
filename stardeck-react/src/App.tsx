def4594a841822651d3dbb80462867c9a12c591c
Eicon <lnemixg@gmail.com>
2025-12-13 21:10:01 +0500

    refactor(app): lite refactor App component

--- stardeck-react/src/App.tsx
+++ stardeck-react/src/App.tsx
@@ -1,16 +1,18 @@
-import './index.css';
-// import s from "./App.module.css";
 import RewardEngine from "./engines/RewardEngine.tsx";
-import StarDeckScene from "./components/NewUIDev/scene/StarDeckScene.tsx";
+import StarDeckScene from "@/components/scene/StarDeckScene.tsx";
+import ModalTaskForm from "@/components/tasks/ModalTaskForm.tsx";
 
 // Уже внедряю графику
 
 export default function App() {
     return (
-        <>
+        <div className="min-h-screen bg-bg-deep text-slate-50 flex flex-col">
             <RewardEngine/>
-
-            <StarDeckScene/>
-        </>
+            <main className="flex-1 flex items-center justify-center">
+                
+                <StarDeckScene/>
+                <ModalTaskForm/>
+            </main>
+        </div>
     );
 }
