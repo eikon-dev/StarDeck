7aa22a2975972cfb0fad101c4a8f403fea1580e6
Eicon <lnemixg@gmail.com>
2025-12-26 20:32:07 +0500

    chore(config): add Tailwind and vite-plugin-glsl

--- stardeck-react/vite.config.ts
+++ stardeck-react/vite.config.ts
@@ -1,7 +1,19 @@
+import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from "@tailwindcss/vite";
+import glsl from 'vite-plugin-glsl';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [
+        react(),
+        tailwindcss(),
+        glsl(),
+    ],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
 })
