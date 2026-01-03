201d5728b998476f04539dcc8e14688a0969d2dd
Eicon <lnemixg@gmail.com>
2026-01-03 21:57:45 +0500

    feat(store): add FX queue with history-ready structure

--- stardeck-react/src/store/useFXStore.ts
+++ stardeck-react/src/store/useFXStore.ts
@@ -1,27 +1,47 @@
 import {create} from "zustand";
 
-type FXType = 'star' | 'shake';
-type FXStatus = 'queued' | 'playing' | 'done';
+type FXType = 'star' | 'shake'; //виды анимаций
+type FXStatus = 'queued' | 'playing' | 'done'; //статусы анимации очередь -> проигрывание -> завершено
 
+
+//это заявка на проигрывание анимации для проигрывателя FSM
 type EffectItem = {
     id: string,
-    type: FXType,
+    type: FXType, //тип эффекта, который должен проиграть FSM
     status: FXStatus,
     createdAt: number,
+    payload: {
+        posY: number,
+        posX: number,
+    }
+    //TODO: Добавить Payload: {}, он говорит как показывать анимацию отдает стартовые параметры
 }
 
 
 interface FXStore {
-    queue: EffectItem[],
+    queue: EffectItem[], //Сюда записываем очередь для воспроизведения
+    history: EffectItem[], //История для проигранных эффектов
 
-    createEffectItem: (id: number) => void,
+    createEffectItem: (newEffectItem: EffectItem) => void, //записывает в очередь заявку на воспроизведение анимации
 }
 
 const useFXStore = create<FXStore>((set) => ({
     queue: [],
+    history: [],
 
-    createEffectItem: (id) => {
+    createEffectItem: (newEffectItem: EffectItem) => {
 
+        const effectItem: EffectItem = {
+            id: crypto.randomUUID?.() ?? String(Date.now() + Math.random()),
+            type: newEffectItem.type,
+            status: 'queued',
+            createdAt: Date.now(),
+            payload: newEffectItem.payload,
+        }
+
+        set((s) => ({
+            queue: [...s.queue, effectItem],
+        }))
     }
 }));
 
