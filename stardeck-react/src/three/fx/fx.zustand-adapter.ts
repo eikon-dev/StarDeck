55b7cadbfa9b07185797c4a3b24ffef762725c67
Eicon <lnemixg@gmail.com>
2026-01-09 21:19:39 +0500

    feat(fx): add FXPort adapter for zustand store

---
+++ stardeck-react/src/three/fx/fx.zustand-adapter.ts
@@ -0,0 +1,20 @@
+import type {FXPort} from "@/three/fx/fx.port";
+import type {EffectItem} from "@/three/fx/fx.type";
+import useFXStore from "@/store/useFXStore";
+
+
+//Plain Object
+const FXZustandAdapter: FXPort = {
+    getQueue(): EffectItem[] {
+        //Маловероятно, но может пригодиться TypeGuard
+        return useFXStore.getState().queue;
+    },
+    start(id: string){
+        useFXStore.getState().start(id);
+    },
+    finish(id: string) {
+        useFXStore.getState().finish(id);
+    },
+}
+
+export default FXZustandAdapter;
