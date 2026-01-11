8e793e3f68148d9f124b1ea0a6782b100cdf9cd6
Eicon <lnemixg@gmail.com>
2026-01-11 21:14:00 +0500

    feat(fx-type): add FXMeta and FXRequest

--- stardeck-react/src/three/fx/fx.type.ts
+++ stardeck-react/src/three/fx/fx.type.ts
@@ -1,27 +1,23 @@
-// export type FXType = 'star' | 'shake'; //виды анимаций
 export type FXStatus = 'queued' | 'playing' | 'done'; //статусы анимации очередь -> проигрывание -> завершено
 
-export type StarEffectItem = {
+//это заявка на проигрывание анимации для проигрывателя FSM
+export type FXMeta = {
     id: string,
-    type: 'star', //тип эффекта, который должен проиграть FSM
     status: FXStatus,
     createdAt: number,
-    payload: {
-        posY: number,
-        posX: number,
-    },
 };
 
-export type ShakeEffectItem = {
-    id: string,
-    type: 'shake', //тип эффекта, который должен проиграть FSM
-    status: FXStatus,
-    createdAt: number,
+export type FXRequest = {
+    type: 'star',
+    payload: {
+        posX: number,
+        posY: number,
+    },
+} | {
+    type: 'shake',
     payload: {
         intensity: number,
         durationMs: number,
     },
-};
-
-//это заявка на проигрывание анимации для проигрывателя FSM
-export type EffectItem = StarEffectItem | ShakeEffectItem ;
+}
+export type EffectItem = FXMeta & FXRequest;
