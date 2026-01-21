fc6bec73549ed39a6ae03227a8a4bac382fec308
Eicon <lnemixg@gmail.com>
2026-01-21 21:06:54 +0500

    feat(persist): add zustand persist for tasks and stars

--- stardeck-react/src/store/useStarsStore.ts
+++ stardeck-react/src/store/useStarsStore.ts
@@ -1,8 +1,15 @@
 import {create} from "zustand";
 import type {StarReward} from "@/types/rewards";
+import {persist} from "zustand/middleware";
+
+//TODO: Current version v1, wait v2
+//TODO: применить другой подход в реализации createStar, использовать другую типизацию
 
 interface StarsStore {
     stars: StarReward[],
+    hasHydrated: boolean,
+
+    setHasHydrated: () => void,
 
     createStar: (input: NewStarsInput) => StarReward,
     addReward: (input: NewStarsInput) => void,
@@ -24,60 +31,83 @@ type LongRewardInput = {
 
 type NewStarsInput = DailyRewardInput | LongRewardInput;
 
-const useStarsStore = create<StarsStore>((set, get) => ({
-    stars: [],
+const useStarsStore = create<StarsStore>()(
+    persist(
+        (set, get) => ({
+            stars: [],
 
-    createStar: (starInput: NewStarsInput) => {
-        if (starInput.kind === 'daily-all') {
-            return {
-                id: crypto.randomUUID?.() ?? String(Date.now() + Math.random()),
-                createdAt: Date.now(),
-                kind: starInput.kind,
-                dayKey: starInput.dayKey, // dayKey это результат выполнения функции проверки dailyDone
-                amount: starInput.amount,
-            }
-        } else {
-            return {
-                id: crypto.randomUUID?.() ?? String(Date.now() + Math.random()),
-                taskId: starInput.taskId,
-                createdAt: Date.now(),
-                kind: starInput.kind,
-                amount: starInput.amount,
+            hasHydrated: false,
+
+            setHasHydrated: () => set({hasHydrated: true}),
+
+            createStar: (starInput: NewStarsInput) => {
+                if (starInput.kind === 'daily-all') {
+                    return {
+                        id: crypto.randomUUID?.() ?? String(Date.now() + Math.random()),
+                        createdAt: Date.now(),
+                        kind: starInput.kind,
+                        dayKey: starInput.dayKey, // dayKey это результат выполнения функции проверки dailyDone
+                        amount: starInput.amount,
+                    }
+                } else {
+                    return {
+                        id: crypto.randomUUID?.() ?? String(Date.now() + Math.random()),
+                        taskId: starInput.taskId,
+                        createdAt: Date.now(),
+                        kind: starInput.kind,
+                        amount: starInput.amount,
+                    }
+                }
+            },
+
+            addReward: (starInput: NewStarsInput) => {
+                const checkDaily = get().hasDailyReward;
+                const checkLong = get().hasLongReward;
+                const createStar = get().createStar;
+
+                if (starInput.kind === 'daily-all') {
+                    if (!checkDaily(starInput.dayKey)) {
+                        const star: StarReward = createStar(starInput)
+                        set(s => ({
+                            stars: [...s.stars, star]
+                        }))
+                    }
+                }
+
+                if (starInput.kind === 'long-complete') {
+                    if (!checkLong(starInput.taskId)) {
+                        const star: StarReward = createStar(starInput)
+                        set(s => ({
+                            stars: [...s.stars, star]
+                        }))
+                    }
+                }
+            },
+
+            hasDailyReward: (dayKey: string) => {
+                return get().stars.some(star => star.dayKey === dayKey && star.kind === 'daily-all')
+            },
+
+            hasLongReward: (taskId: string) => {
+                return get().stars.some(star => star.taskId === taskId && star.kind === 'long-complete')
+            },
+        }), {
+            name: "stars-store",
+
+            partialize: (state) => ({
+                stars: state.stars
+            }),
+
+            onRehydrateStorage: () => (state, error) => {
+                if (error) {
+                    console.error(error);
+                    return;
+                }
+
+                state?.setHasHydrated();
             }
         }
-    },
-
-    addReward: (starInput: NewStarsInput) => {
-        const checkDaily = get().hasDailyReward;
-        const checkLong = get().hasLongReward;
-        const createStar = get().createStar;
-
-        if (starInput.kind === 'daily-all') {
-            if (!checkDaily(starInput.dayKey)) {
-                const star: StarReward = createStar(starInput)
-                set(s => ({
-                    stars: [...s.stars, star]
-                }))
-            }
-        }
-
-        if (starInput.kind === 'long-complete') {
-            if (!checkLong(starInput.taskId)) {
-                const star: StarReward = createStar(starInput)
-                set(s => ({
-                    stars: [...s.stars, star]
-                }))
-            }
-        }
-    },
-
-    hasDailyReward: (dayKey: string) => {
-        return get().stars.some(star => star.dayKey === dayKey && star.kind === 'daily-all')
-    },
-
-    hasLongReward: (taskId: string) => {
-        return get().stars.some(star => star.taskId === taskId && star.kind === 'long-complete')
-    },
-}))
+    )
+)
 
 export default useStarsStore;
--- stardeck-react/src/store/useTasksStore.ts
+++ stardeck-react/src/store/useTasksStore.ts
@@ -1,16 +1,23 @@
 import {create} from 'zustand';
+import {persist} from "zustand/middleware";
 import type {NewTaskInput, Task} from "@/types/task";
 import useEventStore from "./useEventStore.ts";
 
+//TODO: Current version v1, wait v2
+//TODO: Закрыть технический долг resetDailyCycle()
+//TODO: Добавить версионирование
+
 interface TaskStore {
     tasks: Task[],
+    hasHydrated: boolean,
+
+    setHasHydrated: () => void,
 
     addTask: (input: NewTaskInput) => void,
     removeTask: (id: string) => void,
     toggleTask: (id: string) => void,
     resetDailyCycle: () => void,
 }
-//TODO: Закрыть технический долг resetDailyCycle()
 
 // будущее расширение store
 // interface TaskStore {
@@ -24,62 +31,80 @@ interface TaskStore {
 //     checkDailyCompletion: () => void
 // }
 
-const useTasksStore = create<TaskStore>((set, get) => ({
+const useTasksStore = create<TaskStore>()(
+    persist(
+        (set, get) => ({
 
-    tasks: [],
+            tasks: [],
+            hasHydrated: false,
 
+            setHasHydrated: () => set({hasHydrated: true}),
 
-    addTask: (taskInput: NewTaskInput) => {
-        const task: Task = {
-            id: crypto.randomUUID?.() ?? String(Date.now() + Math.random()),
-            title: taskInput.title,
-            description: taskInput.description ?? null,
-            done: false,
-            createdAt: Date.now(),
-            priority: taskInput.priority,
-            cycle: taskInput.cycle,
-        }
+            addTask: (taskInput: NewTaskInput) => {
+                const task: Task = {
+                    id: crypto.randomUUID?.() ?? String(Date.now() + Math.random()),
+                    title: taskInput.title,
+                    description: taskInput.description ?? null,
+                    done: false,
+                    createdAt: Date.now(),
+                    priority: taskInput.priority,
+                    cycle: taskInput.cycle,
+                }
 
-        set(s => ({
-            tasks: [...s.tasks, task]
-        }))
-    },
+                set(s => ({
+                    tasks: [...s.tasks, task]
+                }))
+            },
 
-    removeTask: (id: string) => {
-        set(s => ({
-            tasks: s.tasks.filter(t => t.id !== id)
-        }))
-    },
+            removeTask: (id: string) => {
+                set(s => ({
+                    tasks: s.tasks.filter(t => t.id !== id)
+                }))
+            },
 
-    toggleTask: (id: string) => {
-        const task = get().tasks.find(t => t.id === id);
-        if (!task) return; // чтоб TS не ругался TaskItem всегда передает task.id
+            toggleTask: (id: string) => {
+                const task = get().tasks.find(t => t.id === id);
+                if (!task) return; // чтоб TS не ругался TaskItem всегда передает task.id
 
-        const { done: prevDone, cycle } = task; //де структуризация лаконично и красиво
+                const {done: prevDone, cycle} = task; //де структуризация лаконично и красиво
 
-        set(s => ({
-            tasks: s.tasks.map(t => (t.id === id ? {...t, done: !t.done} : t))
-        }))
+                set(s => ({
+                    tasks: s.tasks.map(t => (t.id === id ? {...t, done: !t.done} : t))
+                }))
 
-        // if (prevDone === undefined) return;
-        // ненужен потому что TaskItem Всегда передает id задачи
-        //     function handleToggle() {
-        //         onToggle(task.id)
-        //     }
+                // if (prevDone === undefined) return;
+                // ненужен потому что TaskItem Всегда передает id задачи
+                //     function handleToggle() {
+                //         onToggle(task.id)
+                //     }
 
-        const nextDone = !prevDone;
+                const nextDone = !prevDone;
 
-        if (nextDone !== prevDone) {
-            useEventStore.getState().emitTaskToggled(id, nextDone, cycle)
-        }
+                if (nextDone !== prevDone) {
+                    useEventStore.getState().emitTaskToggled(id, nextDone, cycle)
+                }
 
-    },
+            },
 
+            resetDailyCycle: () => {
 
-    resetDailyCycle: () => {
+            },
 
-    },
+        }),
+        {
+            name: "tasks-store",
+            partialize: (state) => ({
+                tasks: state.tasks
+            }),
+            onRehydrateStorage: () => (state, error) => {
+                if (error) {
+                    console.error(error);
+                    return;
+                }
 
-}))
+                state?.setHasHydrated();
+            },
+        })
+)
 
 export default useTasksStore;
