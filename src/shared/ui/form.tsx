b28904a3a758f54efa1757533676703855ffade4
Eikon <lnemixg@gmail.com>
2026-02-05 20:55:19 +0500

    refactor(shared/ui): update imports

--- src/shared/ui/form.tsx
+++ src/shared/ui/form.tsx
@@ -13,7 +13,7 @@ import {
   type FieldValues,
 } from "react-hook-form"
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/shared/lib"
 import { Label } from "@/shared/ui/label"
 
 const Form = FormProvider
--- src/shared/ui/item.tsx
+++ src/shared/ui/item.tsx
@@ -2,7 +2,7 @@ import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/shared/lib"
 import { Separator } from "@/shared/ui/separator"
 
 function ItemGroup({ className, ...props }: React.ComponentProps<"div">) {
