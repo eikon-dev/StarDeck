afcc5c9225885ded247f870a6e4a87056d233203
Eikon <lnemixg@gmail.com>
2026-02-05 20:55:19 +0500

    refactor(shared/ui): update imports

--- src/shared/ui/field.tsx
+++ src/shared/ui/field.tsx
@@ -1,7 +1,7 @@
 import { useMemo } from "react"
 import { cva, type VariantProps } from "class-variance-authority"
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/shared/lib"
 import { Label } from "@/shared/ui/label"
 import { Separator } from "@/shared/ui/separator"
 
