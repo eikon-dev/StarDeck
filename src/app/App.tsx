795586efa332b63784ded3bde8bdcddc75d94f84
Eikon <lnemixg@gmail.com>
2026-02-08 16:08:01 +0500

    refactor(app): relocate App to app/

---
+++ src/app/App.tsx
@@ -0,0 +1,19 @@
+import {RewardEngine} from "@/processes/rewards";
+import StarDeckScene from "@/pages/scene/StarDeckScene.tsx";
+import ModalTaskForm from "@/widgets/task-modal/ui/ModalTaskForm.tsx";
+import {ResetDailyEngine} from "@/processes/reset-day";
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-bg-deep text-slate-50 flex flex-col">
+      <ResetDailyEngine/>
+
+      <RewardEngine/>
+      <main className="flex-1 flex items-center justify-center">
+
+        <StarDeckScene/>
+        <ModalTaskForm/>
+      </main>
+    </div>
+  );
+}
