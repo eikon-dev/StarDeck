1f46ad701937679be6848255d5627b6b4daa2814
Eikon <lnemixg@gmail.com>
2026-02-08 16:34:25 +0500

    style(entites/task): index.ts formatted indents

--- src/entities/task/index.ts
+++ src/entities/task/index.ts
@@ -6,9 +6,9 @@ export type {
   Priority
 } from './model/types';
 
-export {useTasksStore} from './model/useTasksStore';
-export {useSortedTasks} from './model/hooks/useSortedTasks'
-export {PRIORITY_LABEL} from "./model/priorityLabel";
-export {selectDailyStats} from "./model/selectors/selectDailyStats";
-export {selectTasksSort} from "./model/selectors/selectTasksSort";
+export {useTasksStore}     from './model/useTasksStore';
+export {useSortedTasks}    from './model/hooks/useSortedTasks';
+export {PRIORITY_LABEL}    from "./model/priorityLabel";
+export {selectDailyStats}  from "./model/selectors/selectDailyStats";
+export {selectTasksSort}   from "./model/selectors/selectTasksSort";
 export {selectTasksFilter} from "./model/selectors/selectTasksFilter";
