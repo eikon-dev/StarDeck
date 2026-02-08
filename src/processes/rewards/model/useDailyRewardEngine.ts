b640e10c07fa4992d88b6bbd7697c851588e93e2
Eikon <lnemixg@gmail.com>
2026-02-08 13:51:53 +0500

    refactor(processes/rewards): decouple long and daily engines via grantReward

--- src/processes/rewards/model/useDailyRewardEngine.ts
+++ src/processes/rewards/model/useDailyRewardEngine.ts
@@ -1,31 +1,31 @@
 import {useEffect} from "react";
 import {useTasksStore, selectDailyStats} from '@/entities/task';
-import {useStarsStore} from "@/entities/reward";
-import useFXStore from "@/entities/fx/model/useFXStore";
+import {useFXStore} from "@/entities/fx";
+import {getDayKey} from "@/shared/lib";
+import {grantReward} from "@/processes/rewards/model/grantReward";
+
 
 //TODO: Current version v1, wait v2
-//TODO: Внести логические улучшения addReward & createEffectItem можно объединить в одну функцию grantDailyReward() использовать внутри useEffect
-//TODO: Баг с появлением награды после resetDailyCycle при первом заходе
+
 export default function useDailyRewardEngine() {
   const tasks = useTasksStore(s => s.tasks);
   const lastResetDailyDayKey = useTasksStore(s => s.lastResetDailyDayKey);
-  const {addReward, hasDailyReward} = useStarsStore.getState()
   const {createEffectItem} = useFXStore.getState();
 
-  useEffect(() => {
-    //TODO: Вынести логику из селектора в движок
-    const {shouldRewardDaily, dayKey} = selectDailyStats(tasks);
+  function grantDailyReward() {
+    const {shouldRewardDaily} = selectDailyStats(tasks);
+    const dayKey = getDayKey();
 
-    //guard clauses
-    if (lastResetDailyDayKey !== dayKey) return;
-    if (!shouldRewardDaily) return;
-    if (hasDailyReward(dayKey)) return;
+    if(!shouldRewardDaily) return;
+    if(lastResetDailyDayKey !== dayKey) return;
 
-    addReward({
-      amount: 1,
+    const grant = grantReward({
+      kind: "daily-all",
       dayKey,
-      kind: 'daily-all',
-    })
+      amount: 1
+    });
+
+    if(!grant) return;
 
     createEffectItem({
       type: 'star',
@@ -34,5 +34,10 @@ export default function useDailyRewardEngine() {
         posY: -5,
       }
     })
-  }, [tasks])
+  }
+
+  useEffect(() => {
+    grantDailyReward();
+
+  }, [tasks, lastResetDailyDayKey])
 }
