17988398538ccbd71e7c5123ad48f48ea57cef27
Eikon <lnemixg@gmail.com>
2026-02-08 12:12:37 +0500

    refactor(entities/task/selectors): remove getDayKey dependency from selectDailyStats

--- src/entities/task/model/selectors/selectDailyStats.ts
+++ src/entities/task/model/selectors/selectDailyStats.ts
@@ -1,11 +1,9 @@
 import {type Task} from '@/entities/task';
-import {getDayKey} from "@/shared/lib";
 
 interface DailyStats {
   totalDaily: number;
   doneDaily: number;
   shouldRewardDaily: boolean;
-  dayKey: string;
 }
 
 //TODO: Решить вопрос с dayKey, это уже не "чистая" функция
@@ -13,13 +11,11 @@ export function selectDailyStats(tasks: Task[]): DailyStats {
   const dailyTasks = tasks.filter(t => t.cycle === 'daily');
   const totalDaily = dailyTasks.length;
   const doneDaily = dailyTasks.filter(t => t.done).length;
-  const dayKey = getDayKey();
   const shouldRewardDaily = totalDaily > 0 && doneDaily === totalDaily;
 
   return {
     totalDaily,
     doneDaily,
     shouldRewardDaily,
-    dayKey,
   }
 }
