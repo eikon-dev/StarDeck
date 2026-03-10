a51595adce714a6c73869562e7ff80210c0ba757
Eikon <lnemixg@gmail.com>
2026-03-10 07:35:34 +0500

    feat(EIK-17): Create User prisma repository

---
+++ apps/api/src/modules/user/infrastructure/user.prisma.repository.ts
@@ -0,0 +1,45 @@
+import {UserRepository} from "../domain/user.repository";
+import {User} from "../domain/user.entity";
+import {PrismaService} from "../../../shared/database/prisma.service";
+
+export class UserPrismaRepository implements UserRepository {
+
+  constructor(
+    private readonly prisma: PrismaService,
+  ) {}
+
+  async findByTelegramId(telegramId: string): Promise<User | null> {
+
+    const record = await this.prisma.user.findUnique({
+      where: {telegramId},
+    })
+
+    if (!record) return null;
+
+    return new User(
+      record.id,
+      record.telegramId,
+      record.username,
+      record.firstName,
+      record.createdAt,
+    )
+  }
+
+  async save(user: User): Promise<void> {
+    await this.prisma.user.upsert({
+      where: {
+        telegramId: user.telegramId
+      },
+      update: {
+        username: user.username,
+        firstName: user.firstName
+      },
+      create: {
+        id: user.id,
+        telegramId: user.telegramId,
+        username: user.username,
+        firstName: user.firstName,
+      },
+    })
+  }
+}
