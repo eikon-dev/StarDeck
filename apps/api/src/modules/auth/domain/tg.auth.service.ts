011b863ac03970143bfed661e3cfc1eb91c69a89
Eikon <lnemixg@gmail.com>
2026-03-12 21:47:26 +0500

    chore: apply code review suggestions for tg-auth service

--- apps/api/src/modules/auth/domain/tg.auth.service.ts
+++ apps/api/src/modules/auth/domain/tg.auth.service.ts
@@ -1,30 +1,30 @@
-import {TelegramAuthData} from "./tg-auth.types";
-import {createHash, createHmac} from "node:crypto";
+import { TelegramAuthData } from './tg-auth.types';
+import crypto from 'crypto';
 
 export class TgAuthService {
   private readonly secretKey: string;
 
-  constructor() {
+  public constructor() {
     this.secretKey = process.env.TELEGRAM_AUTH_SECRET_KEY!;
   }
 
-  verifyHash(authData: TelegramAuthData): boolean {
-    const checkHash = authData.hash;
-
+  public verifyHash(authData: TelegramAuthData): boolean {
     const dataCheckString = Object.keys(authData)
-      .filter(key => key !== 'hash')
+      .filter((key) => key !== 'hash')
       .sort()
-      .map(key => `${key}=${authData[key]}`)
+      .map((key) => `${key}=${authData[key]}`)
       .join('\n');
 
-    const secretKey = createHmac('sha256', "WebAppData")
+    const secretKey = crypto
+      .createHmac('sha256', 'WebAppData')
       .update(this.secretKey)
       .digest();
 
-    const computedHash = createHmac('sha256', secretKey)
+    const computedHash = crypto
+      .createHmac('sha256', secretKey)
       .update(dataCheckString)
       .digest('hex');
 
-    return checkHash === computedHash
+    return authData.hash === computedHash;
   }
 }
