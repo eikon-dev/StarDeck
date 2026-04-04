c3c4d9de6d88cba4806205802e108163808b7ec1
Eikon <lnemixg@gmail.com>
2026-04-04 19:44:30 +0500

    feat(shared): add requireEnv utility

---
+++ apps/api/src/shared/require-env.ts
@@ -0,0 +1,9 @@
+export const requireEnv = (key: string) => {
+  const value = process.env[key];
+
+  if (!value) {
+    throw new Error(`Environment variable ${key} is not defined`);
+  }
+
+  return value;
+};
