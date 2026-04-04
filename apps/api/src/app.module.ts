43271690ee3c4a5662d0ef73870190be2db37896
Eikon <lnemixg@gmail.com>
2026-04-04 19:40:08 +0500

    feat(app): register AuthModule and ConfigModule

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+import { AuthModule } from 'modules/tg-auth/auth.module';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
