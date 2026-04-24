870fc27bd04033e4d57e163b14b62b1e3506388d
Eikon <lnemixg@gmail.com>
2026-04-24 09:25:11 +0500

    chore(config): add prettier, update tsconfig and next config

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ['abnormal-upbeat-charger.ngrok-free.dev'],
 };
 
 export default nextConfig;
