dc3a238837449a08aef9319056e20cff6db3b969
Eikon <lnemixg@gmail.com>
2026-05-21 14:54:19 +0500

    chore(docker): correct next build

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,12 +1,13 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  output: 'standalone',
   allowedDevOrigins: ['abnormal-upbeat-charger.ngrok-free.dev'],
   async rewrites() {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:3001/:path*',
+        destination: `${process.env.API_URL || 'http://localhost:3001'}/:path*`,
       },
     ];
   },
