d8d004921f6285f10329796516344de97c24abc2
Eikon <lnemixg@gmail.com>
2026-06-03 14:10:13 +0500

    fix(web): set black background to prevent white flash on route change

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
         manrope.variable,
       )}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col bg-black">{children}</body>
     </html>
   );
 }
